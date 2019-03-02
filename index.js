d629a46c96ba7bc39942d37c7a178fbb6f5b3a35
吴贻韡 <yiwei.wu@ipiaoniu.com>
2019-03-02 14:14:38 +0800

    feat: 修复

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const IGNORE_PATTERNS = ['.*CHANGELOG', '.*pubspec.yaml']
 const Detector = require('./lib/detector')
 
-module.exports = function app(repository_path, branchs) {
-  detector = new Detector(repository_path, IGNORE_PATTERNS)
+module.exports = function app(repository_path, branchs = []) {
+  let detector = new Detector(repository_path, branchs, IGNORE_PATTERNS)
   detector.pullAllBrachesLocaly()
   let conflicts = detector.get_merge_conflicts()
   return conflicts
